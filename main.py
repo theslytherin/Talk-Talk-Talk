b9e420d448aa3968657fd1f8e8bc654e7ac38ade
theslytherin <anuskasinha1200@gmail.com>
2020-07-25 01:16:58 +0530

    Initial Commit - Part 1

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS
+
+
+def speak(text):
+    tts = gTTS(text=text, lang="en")
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+
+
+speak("Hello Anuska")
